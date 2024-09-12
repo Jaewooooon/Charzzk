ce18e6d5c70d4693cf682cc28f96af53be5607ca
JaewonChoi <wefhio1985@gmail.com>
2024-09-13 00:26:03 +0900

    [Chore]: testcontainers 테스트에서 제거

--- backend/src/test/java/com/ssafy/charzzk/domain/user/respository/UserRepositoryTest.java
+++ backend/src/test/java/com/ssafy/charzzk/domain/user/respository/UserRepositoryTest.java
@@ -6,6 +6,7 @@ import com.ssafy.charzzk.domain.user.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
+@ActiveProfiles("test")
 class UserRepositoryTest extends IntegrationTestSupport {
 
     @Autowired
