f78a555a511013596c1dbcc232788f6312070354
JaewonChoi <wefhio1985@gmail.com>
2024-09-23 14:32:57 +0900

    [Chore]: 배포 도메인 CORS 허용 설정 추가

--- backend/src/main/java/com/ssafy/charzzk/core/configuration/WebConfig.java
+++ backend/src/main/java/com/ssafy/charzzk/core/configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:5173", "https://j11c208.p.ssafy.io/")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
