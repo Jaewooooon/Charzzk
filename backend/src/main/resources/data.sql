0017fc0474e9998736c20c4232aae78aecd04cfa
JaewonChoi <wefhio1985@gmail.com>
2024-09-23 09:51:45 +0900

    [Chore]: 서버가 시작할 때 차종 정보 데이터베이스에 추가

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO car_type (name, image) VALUES
+       ('현대 포터2 일렉트릭', '/cars/hyundai_porter2.png'),
+       ('현대 아이오닉5', '/cars/hyundai_ioniq5.png'),
+       ('현대 아이오닉6', '/cars/hyundai_ioniq6.png'),
+       ('현대 넥쏘', '/cars/hyundai_nexo.png'),
+       ('현대 코나 일렉트릭', '/cars/hyundai_kona_electric.png'),
+       ('기아 EV6', '/cars/kia_ev6.png'),
+       ('기아 EV9', '/cars/kia_ev9.png'),
+       ('기아 봉고3 EV', '/cars/kia_bongo3_ev.png'),
+       ('기아 니로 EV', '/cars/kia_niro_ev.png'),
+       ('기아 레이 EV', '/cars/kia_ray_ev.png'),
+       ('테슬라 모델Y', '/cars/tesla_model_y.png'),
+       ('테슬라 모델3', '/cars/tesla_model_3.png');
