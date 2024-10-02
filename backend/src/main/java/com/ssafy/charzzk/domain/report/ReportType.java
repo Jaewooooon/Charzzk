0230a959828a7eb86d712da0a822f2dcd9c88bbd
지예찬 <blackhaze529@naver.com>
2024-10-02 14:07:33 +0900

    [Refactor]: ReportType 추가

--- backend/src/main/java/com/ssafy/charzzk/domain/report/ReportType.java
+++ backend/src/main/java/com/ssafy/charzzk/domain/report/ReportType.java
@@ -4,5 +4,6 @@ public enum ReportType {
     FLIPPED,        // 로봇이 뒤집혀 있는 경우
     BROKEN,         // 로봇이 고장난 경우 (미작동, 충전 불가 등)
     DAMAGED,        // 로봇의 외관이 손상된 경우 (부품이 빠졌다거나...)
+    NOT_CHARGING,   // 충전이 안됨.
     ETC;            // 기타 신고
 }
