6c18188b8267d7c8687bc4008bec508d891d6657
JaewonChoi <wefhio1985@gmail.com>
2024-10-10 16:23:43 +0900

    [Chore]: 불필요한 의존성 제거

--- backend/src/main/java/com/ssafy/charzzk/api/service/reservation/ReservationService.java
+++ backend/src/main/java/com/ssafy/charzzk/api/service/reservation/ReservationService.java
@@ -135,26 +135,26 @@ public class ReservationService {
         return canceledReservation;
     }
 
-//    public List<ReservationQueueResponse> getReservations() {
-//        Map<Long, Queue<Reservation>> reservationMap = reservationManager.getReservationQueueMap();
-//
-//        for (Map.Entry<Long, Queue<Reservation>> l : reservationMap.entrySet()) {
-//            System.out.println(l.getKey());
-//            for (Reservation r : l.getValue()) {
-//                System.out.println(r);
-//            }
-//        }
-//
-//        List<ReservationQueueResponse> response = new ArrayList<>();
-//
-//        for (Map.Entry<Long, Queue<Reservation>> entry : reservationMap.entrySet()) {
-//            ReservationQueueResponse.of(entry.getKey(), entry.getValue().stream().map(ReservationResponse::from).collect(Collectors.toList()));
-//        }
-//
-//        return response;
-//    }
-//
-//    public void deleteReservations() {
-//        reservationManager.deleteAllReservations();
-//    }
+    public List<ReservationQueueResponse> getReservations() {
+        Map<Long, Queue<Reservation>> reservationMap = reservationManager.getReservationQueueMap();
+
+        for (Map.Entry<Long, Queue<Reservation>> l : reservationMap.entrySet()) {
+            System.out.println(l.getKey());
+            for (Reservation r : l.getValue()) {
+                System.out.println(r);
+            }
+        }
+
+        List<ReservationQueueResponse> response = new ArrayList<>();
+
+        for (Map.Entry<Long, Queue<Reservation>> entry : reservationMap.entrySet()) {
+            ReservationQueueResponse.of(entry.getKey(), entry.getValue().stream().map(ReservationResponse::from).collect(Collectors.toList()));
+        }
+
+        return response;
+    }
+
+    public void deleteReservations() {
+        reservationManager.deleteAllReservations();
+    }
 }
